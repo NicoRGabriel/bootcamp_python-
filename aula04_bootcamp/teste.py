29e6f90be6549a909635dc8d488fd29bdae1a933
Nicolas Gabriel <nicolas.rgabriel@gmail.com>
2024-06-19 11:31:03 -0300

    aula04

---
+++ aula04_bootcamp/teste.py
@@ -0,0 +1,10 @@
+# #### Inteiros (`int`)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero_01 = int(input("insira um número: "))
+numero_02 = int(input("insira outro número: "))
+
+valor_total = numero_01 + numero_02
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
