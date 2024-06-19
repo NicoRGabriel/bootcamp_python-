fe51aa2ba85ef33761963e62b1dfbfa4955a49b3
Nicolas Gabriel <nicolas.rgabriel@gmail.com>
2024-06-19 16:19:16 -0300

    adicionado aulas 01 e 02

---
+++ aula01_02_bootcamp/exercicio_aula02.py
@@ -0,0 +1,5 @@
+numero_01 = int(input("Digite um número: "))
+numero_02 = int(input("Digite outro numero número: "))
+resultado = numero_01 + numero_02
+
+print(f"A soma de {numero_01} + {numero_02} é = {resultado}")
