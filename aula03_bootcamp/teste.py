c1130fc360e4d71abd3e44a91ce6abb139c2d0c8
Nicolas Gabriel <nicolas.rgabriel@gmail.com>
2024-06-19 16:21:54 -0300

    reparado o teste

--- aula03_bootcamp/teste.py
+++ aula03_bootcamp/teste.py
@@ -1,5 +1,5 @@
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
-usuario = str(input("Digite seu nome completo"))
+usuario = str(input("Digite o seu nome completo"))
 
 print(usuario.lower())
