6cbdbb27f9e15f030d18f4342994fd77e671670e
Nicolas Gabriel <nicolas.rgabriel@gmail.com>
2024-06-24 15:31:22 -0300

    adicionado exemplo aula 02

--- aula01_02_bootcamp/exercicio_aula02.py
+++ aula01_02_bootcamp/exercicio_aula02.py
@@ -1,5 +1,14 @@
-numero_01 = int(input("Digite um número: "))
-numero_02 = int(input("Digite outro numero número: "))
-resultado = numero_01 + numero_02
+# numero_01 = int(input("Digite um número: "))
+# numero_02 = int(input("Digite outro numero número: "))
+# resultado = numero_01 + numero_02
 
-print(f"A soma de {numero_01} + {numero_02} é = {resultado}")
+# print(f"A soma de {numero_01} + {numero_02} é = {resultado}")
+
+
+
+# verificar tipo de variavel
+numero = int(input("Digite um número: "))
+if isinstance(numero, int):
+    print("A variável é um inteiro.")
+else:
+    print("A variável não é um inteiro.")
