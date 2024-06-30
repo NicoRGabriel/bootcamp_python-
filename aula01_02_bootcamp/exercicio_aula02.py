f77e81b390ec6177b025c91b5b2f0d47549e4d35
Nicolas Gabriel <nicolas.rgabriel@gmail.com>
2024-06-30 11:51:53 -0300

    adicionado exercicio aula02

--- aula01_02_bootcamp/exercicio_aula02.py
+++ aula01_02_bootcamp/exercicio_aula02.py
@@ -1,14 +1,98 @@
-# numero_01 = int(input("Digite um número: "))
-# numero_02 = int(input("Digite outro numero número: "))
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+# numero_01 = int(input("insira um número: "))
+# numero_02 = int(input("insira outro número: "))
+
+# valor_total = numero_01 + numero_02
+
+# print(f"o valor somado é igual a {valor_total}")
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+# usuario = int(input("digite um numero: "))
+# cauculo_divisao = usuario % 5
+
+# print(f" o resto da divisao do numero {usuario} divido por 5 é = {cauculo_divisao}")
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+# numero_01 = int(input("insira um número: "))
+# numero_02 = int(input("insira outro número: "))
+# valor_total = numero_01 * numero_02
+
+# print(f"o valor multiplicado é igual a {valor_total}")
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+# numero_01 = int(input("insira um número: "))
+# numero_02 = int(input("insira outro número: "))
+# cauculo_divisao_interira = numero_01 // numero_02
+
+# print(f"a divisao inteira de {numero_01} por {numero_02} é = {cauculo_divisao_interira}")
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+# numero_01 = int(input("insira um número: "))
+# resultado = numero_01 ** 2
+
+# print(f"o quadrado do número {numero_01} é = {resultado}")
+
+# #### Números de Ponto Flutuante (`float`)
+
+import math
+
+# # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+# numero_01 = float(input("Digite o primeiro numero: "))
+# numero_02 = float(input("Digite o segundo numero:"))
 # resultado = numero_01 + numero_02
 
-# print(f"A soma de {numero_01} + {numero_02} é = {resultado}")
+# print(resultado)
+
+# # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+# numero_01 = float(input("Digite um numero: "))
+# numero_02 = float(input("Digite outro numero: "))
+# media = (numero_01 + numero_02) / 2
+
+# print(media)
+# # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+# raio_do_circulo = float(input("Digite o raio: "))
+# area_do_circulo = math.pi * raio_do_circulo ** 2
+
+# print(f"{area_do_circulo:.2f}")
+
+
+# #### Strings (`str`) 
+
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+
+# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
 
 
-# verificar tipo de variavel
-numero = int(input("Digite um número: "))
-if isinstance(numero, int):
-    print("A variável é um inteiro.")
-else:
-    print("A variável não é um inteiro.")
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+# #### Booleanos (`bool`)
+
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+# #### try-except e if
+
+# 21: Conversor de Temperatura
+# 22: Verificador de Palíndromo
+# 23: Calculadora Simples
+# 24: Classificador de Números
+# 25: Conversão de Tipo com Validação
