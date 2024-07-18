110d3df1e091b2c979d72b07a655cc4c052d85d0
Nicolas Gabriel <nicolas.rgabriel@gmail.com>
2024-07-18 00:10:53 -0300

    exercicios aula 3

--- aula03_bootcamp/teste.py
+++ aula03_bootcamp/teste.py
@@ -1,5 +1,8 @@
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
-usuario = str(input("Digite o seu nome completo"))
+# cadastro = {'nome': 'Nícolas', 'idade': '31', 'sexo': 'masculino' }
 
-print(usuario.lower())
+# print(cadastro['nome'])
+
+
+
